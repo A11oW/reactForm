f34a0567df02230934bf2420d435d9696a14a935
A11oW <toca@inbox.ru>
2015-10-07 15:10:16 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: {
+        main: [
+            'webpack-dev-server/client?http://localhost:8080',
+            './js/app/main.js',
+        ]
+    },
+    output: {
+        filename: './js/build/[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'jsx',
+                exclude: /node_modules/
+            },
+            {
+                test:   /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
+            }
+
+        ]
+    },
+    postcss: function () {
+        return [autoprefixer];
+    }
+};

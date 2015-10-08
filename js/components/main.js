98055adf6f6ec23ea8bcc02edbc4aa44f0359caf
A11oW <toca@inbox.ru>
2015-10-08 12:11:40 +0300

    remove non project files

--- js/components/main.js
+++ js/components/main.js
@@ -3,18 +3,3 @@ import React from 'react';
 import Calc from './calc/calc';
 
 React.render(<Calc />, document.getElementById('root'));
-
-
-/*import HelloWorld from './hello-world/hello-world';
-React.render(
-    <HelloWorld />,
-    document.getElementById('b-calc')
-);*/
-
-
-//import Counter from './counter';
-/*
-React.render(
-    <Counter />,
-    document.getElementById('test')
-);*/
